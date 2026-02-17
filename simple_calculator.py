6544e403b707837821f0abbc17e038eede47c5e4
sameersukhija28112007-beep <sameersukhija28112007@gmail.com>
2026-02-17 20:08:38 +0530

    Refactor calculator to handle negative inputs

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,8 +1,13 @@
 exp=input("enter an arithmetic operation:")
-if exp[0]=="+":
-    exp=exp[1:]
+try:
+    if exp[0]=="+":
+        exp=exp[1:]
+    elif exp[0]=="-":
+        exp="0"+exp
+except:
+    pass
 a=[]
-for i in exp:
+for i in exp[:]:
     if (i=="/"or i=="*"or i=="-"or i=="+") and exp.index(i)>=1 :
         a.append(exp[0:exp.index(i,1)])
         a.append(i)
@@ -46,4 +51,4 @@ try:
 except ValueError:
     print("invalid expression")
 except ZeroDivisionError:
-    print("division by zero not allowed")
+    print("division by zero is not allowed")
